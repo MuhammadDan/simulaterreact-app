d65a299fed527124444068ab35909c57ed557ea4
DanKhan1 <chaudriali034@gmail.com>
2024-12-26 10:42:32 +0500

    this is my second change

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+     <header className="bg-gradient-to-r from-blue-600 to-blue-800 text-white py-4 sticky top-0 shadow-md z-10">
+          <div className="container mx-auto flex justify-between items-center px-4">
+            <h1 className="text-3xl font-bold tracking-wide">
+              Queuing Model Simulator
+            </h1>
+            <nav>
+              <ul className="flex space-x-6">
+                <li>
+                  <a
+                    href="#features"
+                    className="hover:text-yellow-300 transition duration-300"
+                  >
+                    Features
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#how-it-works"
+                    className="hover:text-yellow-300 transition duration-300"
+                  >
+                    How It Works
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#contact"
+                    className="hover:text-yellow-300 transition duration-300"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+    </>
+  )
+}
+
+export default Navbar
